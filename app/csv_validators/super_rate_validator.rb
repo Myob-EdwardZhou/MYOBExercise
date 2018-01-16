18968676c40a9bef34169593a5f7c1fdd55c0ca9
Edward <Edward_mjz@hotmail.com>
2018-01-16 22:31:56 +0800

    Finish super rate validator

--- app/csv_validators/super_rate_validator.rb
+++ app/csv_validators/super_rate_validator.rb
@@ -1,8 +1,19 @@
 class CSVValidator::SuperRateValidator
+  SUPER_RATE_FORMAT = /\A\d+%\z/
+
   def initialize(input_csv)
     @input_csv = input_csv
   end
 
   def validate
+    unless annual_salary_format_valid?
+      raise ValidateInputCSVException, 'invalid_super_rate_format'
+    end
+  end
+
+  private
+
+  def annual_salary_format_valid?
+    @input_csv.super_rates.all? { |super_rate| super_rate =~ SUPER_RATE_FORMAT }
   end
 end
---
+++ spec/csv_validators/super_rate_validator_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe CSVValidator::SuperRateValidator, '#validate' do
+  let(:input_csv) { CSVParser.new(csv_file_path) }
+
+  subject { described_class.new(input_csv).validate }
+
+  context 'with a csv file has invalid annual salary format' do
+    let(:csv_file_path) { 'spec/fixtures/validations/invalid_super_rate.csv' }
+
+    it 'raise invalid annual salary format error' do
+      expect { subject }.to raise_error(ValidateInputCSVException, 'invalid_super_rate_format')
+    end
+  end
+end
