8f798cb0af1a376fab94a1fe8f8c3f02767f72c7
Edward <Edward_mjz@hotmail.com>
2018-01-18 19:39:38 +0800

    Introduce Formula Repository map formula from tax_rule config file and return correct Formula

---
+++ app/repositories/formula_repository.rb
@@ -0,0 +1,17 @@
+require 'singleton'
+require 'yaml'
+
+class FormulaRepository
+  include Singleton
+
+  def find_formula(annual_salary)
+    formulas.find { |formula| annual_salary >= formula.min_income && annual_salary <= formula.max_income }
+  end
+
+  private
+
+  def formulas
+    rule_json_array = YAML.load_file(Monkeyking.project_root.join('config/tax_rules.yml'))['tax_rules']
+    rule_json_array.map { |rule| Formula.new(rule) }
+  end
+end
